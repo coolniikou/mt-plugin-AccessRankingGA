40c7e4106791cd4f6a2a699f487e396a1efab84a
coolniikou <cool.ni.ikou@gmail.com>
2010-11-22 23:40:38 +0900

    changed config.tmpl L10file

--- plugins/AccessRankingGA/lib/AccessRankingGA/L10N/ja.pm
+++ plugins/AccessRankingGA/lib/AccessRankingGA/L10N/ja.pm
@@ -10,12 +10,17 @@ use vars qw( %Lexicon );
 	'as_DESCRIPTION' => 'Google Analyticsよりユニークビジター数のデータを抽出し、JSONデータを生成します。',
 	'as_SET_DES' => 'Google Analytics　ログイン情報が必要です。プロファイルIDはWebサイトのレポート表示の際にURLの中にある数列です。詳細は<a href="http://www.google.com/support/analytics/bin/answer.py?hl=jp&answer=97705" target="_blank">Google ヘルプ › Analytics ヘルプ › 開始方法 › 用語解説 › プロファイル ID：http://www.google.com/support/analytics/bin/answer.py?hl=jp&answer=97705</a>を参照ください。',
 	'as_SET_TITLE' => 'Google Analytics 詳細設定',
-	'as_GA_Username' => 'Google Analytics<br />ユーザ名',
-	'as_GA_Password' => 'Google Analytics<br />パスワード',
-	'as_GA_ProfileID' => 'Google Analytics<br />プロファイル ID',
+	'as_ASTERISK' => '* 印 必須項目',
+	'as_GA_Username' => '* Google Analytics<br />ユーザ名',
+	'as_GA_Password' => '* Google Analytics<br />パスワード',
+	'as_GA_ProfileID' => '* Google Analytics<br />プロファイル ID',
 	'as_GA_Maxresult' => 'エントリー最大表示数<br />（訪問数昇順）',
-	'Cannot get token, please confirm your settigs of Google Analytics login (as of login account or password).' => 'tokenを取得することができません。プラグイン設定画面にて、Google Analytics login情報（ログイン、パスワード）の設定を確認してください。', 
-	'Cannot get contents, please confirm your settigs of Google Analytics login (as of profileID).' => 'アクセス情報を取得することができません。プラグイン設定画面にて、Google Analytics login情報（プロファイルIDもしくは、ログイン、パスワード）の設定を確認してください。',
+	'as_GA_ExcludeTOP' => 'トップページ除外',
+	'as_GA_ExcludeTOP_Hint' => 'もしトップページデータを除外したい場合はチェックしてください。',
+	'as_GA_Exclude_Path' => '除外パスリスト',
+	'as_GA_Exclude_Path_Hint' => '除外したいページパスを入力してください。例）もしphotoshopカテゴリ（パス名）の場合、"photoshop"入力。 ',
+	'as_token_error' => 'Google Analytics DATA API認証が取得できませんでした。プラグイン設定画面にて詳細設定項目の内容を確認ください。', 
+	'as_contents_error' => 'Google Analytics DATA APIからデータ取得できませんでした。詳細設定項目の内容を確認してください。',
 );
 
 1;
