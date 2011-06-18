2bdff9780da6969b77a914cc9e20a9c987747508
coolniikou <cool.ni.ikou@gmail.com>
2011-06-18 17:28:03 +0900

    bug fixed

--- mt-static/js/jquery.rankingtab.js
+++ mt-static/js/jquery.rankingtab.js
@@ -1,31 +1,31 @@
 ;(function($){
         $.fn.rankingtab = function(options) {
 
-			var opts = $.extent({}, $.fn.rankingtab.defaults, options);
+			var opts = $.extend({}, $.fn.rankingtab.defaults, options);
 
 			return this.each(function() {
 				var $rklist = $(this);
 				$('ul.rktab li a', $rklist).bind('click', function() {
-					onclick.call(this, $rklist);
+					onclick.call(this, $rklist, opts);
 					return false;
 				}).filter(':first').click();
 			});
 		}; 
 
-		function onclick($rklist) {
+		function onclick($rklist, opts) {
 			$('li.selected', $rklist).removeClass('selected');
 			var $a = $(this).parent().addClass('selected');
-			get_data($(this).text(), $rklist);
+			get_data($(this).text(), $rklist, opts);
 		}
 
-		function get_data(tag, $rklist) {
+		function get_data(tag, $rklist, opts) {
 			var url = opts.baseurl+tag+'.json';
 			$.getJSON(url, function(a) {
-				show_data(a, $rklist);
+				show_data(a, $rklist, opts);
 			});
 		}
 
-		function show_data(a, $rklist) {
+		function show_data(a, $rklist, opts) {
 			var list = '<ul class="rk_list">';
 			var c = 1;
 			for (i=0; i<a.length; i++) {
@@ -42,7 +42,6 @@
 		}
 
         $.fn.rankingtab.defaults = {
-			baseurl: "<$MTBlgoURL$>",
 			trunc: 40
         };
 
