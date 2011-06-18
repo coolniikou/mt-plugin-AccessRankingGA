5101fecb28abb68b1aaadffe0c937c7d8624cc7d
coolniikou <cool.ni.ikou@gmail.com>
2011-06-18 09:06:49 +0900

    retouch README.md

--- plugins/AccessRankingGA/lib/AccessRankingGA/Plugin.pm
+++ plugins/AccessRankingGA/lib/AccessRankingGA/Plugin.pm
@@ -10,11 +10,13 @@ use Data::Dumper;
 
 ## DEBUC CODE
 sub doLog {
-    my ($msg) = @_;
+    my ( $msg, $class ) = @_;
     return unless defined($msg);
     use MT::Log;
     my $log = MT::Log->new;
     $log->message($msg);
+	$log->level(MT::Log::DEBUG());
+	$log->class($class) if $class;
     $log->save or die $log->errstr;
 }
 
