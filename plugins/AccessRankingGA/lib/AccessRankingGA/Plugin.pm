39ce48d055fa6b1a417e919dd2a3e29c84b04040
coolniikou <cool.ni.ikou@gmail.com>
2012-10-16 17:26:05 +0900

    exec tidy

--- plugins/AccessRankingGA/lib/AccessRankingGA/Plugin.pm
+++ plugins/AccessRankingGA/lib/AccessRankingGA/Plugin.pm
@@ -26,10 +26,8 @@ sub _hdlr_analytic_tags {
     }
 
     my $config = {
-        user =>
-          $plugin->get_config_value( 'GA_username', 'blog:' . $blog_id ),
-        pass =>
-          $plugin->get_config_value( 'GA_password', 'blog:' . $blog_id ),
+        user => $plugin->get_config_value( 'GA_username', 'blog:' . $blog_id ),
+        pass => $plugin->get_config_value( 'GA_password', 'blog:' . $blog_id ),
         profileid =>
           $plugin->get_config_value( 'GA_profile_id', 'blog:' . $blog_id ),
         maxresult =>
@@ -37,7 +35,9 @@ sub _hdlr_analytic_tags {
         exclude_top =>
           $plugin->get_config_value( 'GA_exclude_top', 'blog:' . $blog_id ),
         exclude_optional_path => trim(
-           $plugin->get_config_value( 'GA_exclude_optional_path', 'blog:' . $blog_id )
+            $plugin->get_config_value(
+                'GA_exclude_optional_path', 'blog:' . $blog_id
+            )
         ),
         start => format_ts( '%Y-%m-%d', $ago,   $blog ),
         end   => format_ts( '%Y-%m-%d', $today, $blog ),
@@ -45,15 +45,15 @@ sub _hdlr_analytic_tags {
 
     my $token = &get_token($config);
     my $data = &get_data( $token, $config );
-    return $data if( MT->VERSION < 5 );
+    return $data if ( MT->VERSION < 5 );
 
     $data = decode_utf8($data);
     return $data;
 }
 
 sub get_token {
-    my $config   = shift;
-    my $plugin   = MT->component('AccessRankingGA');
+    my $config = shift;
+    my $plugin = MT->component('AccessRankingGA');
     my $ua =
       MT->new_ua( { agent => join( "/", $plugin->name, $plugin->version ) } );
     my $token_url = 'https://www.google.com/accounts/ClientLogin';
@@ -84,55 +84,54 @@ sub get_token {
 }
 
 sub get_data {
-     my ( $token, $config ) = @_;
-     my $plugin   = MT->component('AccessRankingGA');
-     my $ua =
-       MT->new_ua( { agent => join( "/", $plugin->name, $plugin->version ) } );
-     my $url =
-         "https://www.googleapis.com/analytics/v3/data/ga"
-       . "?ids=ga:"
-       . $config->{profileid}
-       . "&dimensions=ga:pagePath,ga:pageTitle"
-       . "&metrics=ga:uniquePageviews"
-       . "&sort=-ga:uniquePageviews"
-       . "&start-date="
-       . $config->{start}
-       . "&end-date="
-       . $config->{end}
-       . "&max-results="
-       . $config->{maxresult};
-     ## optional exlude settings
-     if ( $config->{exclude_top}
-         || $config->{exclude_optional_path}
-         && $config->{exclude_optional_path} ne '' )
-     {
-           $url .= "&filters=";
-           my @exclude_paths =
-                   ( $config->{exclude_optional_path} =~ /(\S+)/g );
-           for ( @exclude_paths ) {
-               $url .= "ga:pagePath!~". $_ . ";";
-           }
+    my ( $token, $config ) = @_;
+    my $plugin = MT->component('AccessRankingGA');
+    my $ua =
+      MT->new_ua( { agent => join( "/", $plugin->name, $plugin->version ) } );
+    my $url =
+        "https://www.googleapis.com/analytics/v3/data/ga"
+      . "?ids=ga:"
+      . $config->{profileid}
+      . "&dimensions=ga:pagePath,ga:pageTitle"
+      . "&metrics=ga:uniquePageviews"
+      . "&sort=-ga:uniquePageviews"
+      . "&start-date="
+      . $config->{start}
+      . "&end-date="
+      . $config->{end}
+      . "&max-results="
+      . $config->{maxresult};
+    ## optional exlude settings
+    if (   $config->{exclude_top}
+        || $config->{exclude_optional_path}
+        && $config->{exclude_optional_path} ne '' )
+    {
+        $url .= "&filters=";
+        my @exclude_paths = ( $config->{exclude_optional_path} =~ /(\S+)/g );
+        for (@exclude_paths) {
+            $url .= "ga:pagePath!~" . $_ . ";";
+        }
 
-           $url .= "ga:pagePath!~/%24;ga:pagePath!~/index.html%24"
-                   if ( defined($config->{exclude_top}) );
-     }
+        $url .= "ga:pagePath!~/%24;ga:pagePath!~/index.html%24"
+          if ( defined( $config->{exclude_top} ) );
+    }
 
-     my @headers = ( Authorization => "GoogleLogin Auth=$token" );
-     my $res = $ua->get( $url, @headers );
-     if ( $res->is_success ) {
-         return $res->content;
-     }
-     else {
-         MT->log(
-             {
-                 message => $plugin->translate('as_contents_error')
-                   . $res->status_line,
-                 class => 'system',
-                 level => MT::Log::ERROR(),
-             }
-         );
-         die $plugin->translate('as_contents_error') . $res->status_line;
-     }
+    my @headers = ( Authorization => "GoogleLogin Auth=$token" );
+    my $res = $ua->get( $url, @headers );
+    if ( $res->is_success ) {
+        return $res->content;
+    }
+    else {
+        MT->log(
+            {
+                message => $plugin->translate('as_contents_error')
+                  . $res->status_line,
+                class => 'system',
+                level => MT::Log::ERROR(),
+            }
+        );
+        die $plugin->translate('as_contents_error') . $res->status_line;
+    }
 }
 
 1;
